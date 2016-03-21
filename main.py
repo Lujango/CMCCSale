70fc62983ada284628f6729d26a49cb241793098
Lujango <935819994@qq.com>
2016-03-21 14:24:55 +0800

    添加时间操作

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# -*- coding:utf-8 -*-
+#coding:utf-8
 import os
 import urllib
 import urllib2
 import re
-
+import datetime
+from html2text import html2text
+import nltk as nltk
 
 __author__ = 'pro'
 
@@ -32,7 +34,7 @@ def dealdata(content):
                          '/a>.*?</td>.*?<td style="width:100px" align="left">(.*?)</td>',re.S)
 
     items = re.findall(pattern,content)
-    print len(items)
+    print "获取".decode('utf-8').encode('gbk'),len(items)
     return items
 
 
@@ -43,7 +45,7 @@ def getcontent(content):
     results = re.findall(pattern,content)
     for result in results:
         str = str+result+"\n"
-    pattern = re.compile('<span style="font-size: 16px;margin-left:16px;">(.*?)</span>     </div>     </td>   </tr></table></div>',re.S)
+    pattern = re.compile('<td colspan="2">(.*?)</td>.*?</tr>',re.S)
     results = re.findall(pattern,content)
     for result in results:
         result = result.replace('<br />','\n').replace('&nbsp;','').replace('</span>','').replace('</div>','').replace('<div>','')\
@@ -65,26 +67,36 @@ def getinfo(index):
 
 
 def removeall():
+    if not os.path.isdir("result"):
+        os.mkdir("result")
     for eachFile in os.listdir('result'):
         print eachFile
         os.remove('result/'+eachFile)
 
 
 def savefile(title,content):
-    f=open('result/'+title+'.txt','w')
-    f.write(content)
+    f = open('result/'+title+'.txt','w')
+    f.write(content.encode('utf-8'))
     f.close()
 
 
 if __name__ == "__main__":
-    print "开始获取数据......"
-    pagenum = int(raw_input("请输入要获取的页数!(输入数字)"))
+    print "开始获取数据......".decode('utf-8').encode('gbk')
+    pagenum = raw_input("请输入时间!(例如：2016-3-21)".decode('utf-8').encode('gbk'))
     print pagenum
-    content = getdata(pagenum,20)
-    content = content.decode('utf-8')
+    mytime = datetime.datetime.strptime(pagenum,'%Y-%m-%d')
     removeall()
-    for item in dealdata(content):
-        savefile(item[2],getcontent(getinfo(item[0])))
-        print "已写入",item[0],item[1],item[2],item[3]
+    for i in range(1,20):
+        content = getdata(i,20)
+        content = content.decode('utf-8')
+        mytime1 = datetime.datetime.strptime(pagenum,'%Y-%m-%d')
+        for item in dealdata(content):
+            if u"四川" in item[1] or u"重庆" in item[1]:
+                savefile(item[2],html2text(getinfo(item[0]).decode('utf-8')))
+                print "已写入".decode('utf-8').encode('gbk'),item[0],item[1],item[2],item[3]
+            mytime1 = datetime.datetime.strptime(item[3],'%Y-%m-%d')
+        if mytime > mytime1:
+            print '共'.decode('utf-8').encode('gbk'),i,'页'.decode('utf-8').encode('gbk')
+            break
 
 
