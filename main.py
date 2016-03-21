70e916ece58882d6064dffa0ec373e8638d03609
lujango <123456>
2016-03-21 11:04:58 +0800

    文件读写

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import os
 import urllib
 import urllib2
 import re
@@ -31,16 +32,30 @@ def dealdata(content):
 
     items = re.findall(pattern,content)
     print len(items)
-    for item in items:
-        print item[0],item[1],item[2],item[3]
+    return items
+    #for item in items:
+    #    print item[0],item[1],item[2],item[3]
 
 
  # 获取文章详细信息
 def getcontent(content):
+    str = ''
     pattern = re.compile('<h1>(.*?)</h1>',re.S)
     results = re.findall(pattern,content)
     for result in results:
+        str = str+result+"\n"
         print result
+    pattern = re.compile('<span style="font-size: 16px;margin-left:16px;">(.*?)</span>     </div>     </td>   </tr></table></div>',re.S)
+    results = re.findall(pattern,content)
+    for result in results:
+        result = result.replace('<br />','\n').replace('&nbsp;','').replace('</span>','').replace('</div>','').replace('<div>','')\
+            .replace('</td>','').replace('</tr>','').replace('<td  colspan="3">','').replace('<tr>','')\
+            .replace('<span style="font-size: 16px;margin-left:16px;">','\n').replace('<span style="font-size: 16px;font-weight: bold;">','\n')\
+            .replace('<span style="font-weight: bold;font-size: 16px;font-weight: bold;">','\n').replace('<span style="font-size: 16px;">','')\
+            .replace('<a href="http://b2b.10086.cn/">（http://b2b.10086.cn）</a>','').replace('<span style="font-size:16px;float:right; clear:both;">','\n')
+        str = str+result+"\n"
+        print result
+    return str
 
 
 #根据文章索引获取内容
@@ -52,10 +67,19 @@ def getinfo(index):
     return the_page
 
 
+def removeall():
+    for eachFile in os.listdir('result'):
+        print eachFile
+        os.remove('result/'+eachFile)
+
+
 if __name__ == "__main__":
     print "begin"
     #content = getdata(1,20)
     #content = content.decode('utf-8')
     #print content
     #dealdata(content)
-    getcontent(getinfo(252295))
+    #getcontent(getinfo(252295))
+    f=open('result/f.txt','w')
+    f.write(getcontent(getinfo(252295)))
+    f.close()
